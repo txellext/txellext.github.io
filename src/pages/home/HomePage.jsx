3a074d43adfc3e9d7b2b0f3bfe041de0d4f2adeb
Txell Amigó <txellmgsol@gmail.com>
2023-02-23 18:12:29 +0100

    project structure done

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/homePage.css'
+
+const HomePage = () => {
+    return (
+        <div style={{ "background-color":'black' }}>
+            <img src="https://images.pexels.com/photos/3785932/pexels-photo-3785932.jpeg" alt="Work-In-Progress" width="1480" />
+            <button>
+                <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg> Explore me
+            </button>
+        </div>
+    );
+}
+
+export default HomePage;
